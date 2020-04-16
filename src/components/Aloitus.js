0f0494dab022256a99938b310247301d6a1044d0
niinab <niina.blom@myy.haaga-helia.fi>
2020-04-16 12:00:39 +0300

    kuva aloitussivuun

--- src/components/Aloitus.js
+++ src/components/Aloitus.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import Kysymysmerkki from '../kysymysmerkki.jpg';
 
 function Aloitus() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Aloitus splash screen
-        </p>
+        <h1>Tervetuloa!</h1>
+          <img src={Kysymysmerkki} width={1400} height={900} mode='fit'></img>
+          
+        
         
       </header>
     </div>
